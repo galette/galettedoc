38d8d520fef3868a3e594afd0cf229bf9983503b
Johan Cwiklinski <johan@x-tnd.be>
2020-02-13 12:58:21 +0100

    Bump year

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Galette'
 author = u'Johan Cwiklinski'
-copyright = u'2011-2019, Johan Cwiklinski'
+copyright = u'2011-2020, Johan Cwiklinski'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
