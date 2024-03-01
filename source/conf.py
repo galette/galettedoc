7ea5200495119e790c916f390e145c7c816b1a0c
Johan Cwiklinski <johan@x-tnd.be>
2024-03-01 08:42:39 +0100

    Galette 1.0.3

--- source/conf.py
+++ source/conf.py
@@ -56,7 +56,7 @@ copyright = '2011-2023, Johan Cwiklinski'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.2'
+release = '1.0.3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
